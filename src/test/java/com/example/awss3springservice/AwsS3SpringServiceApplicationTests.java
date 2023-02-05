9d459caedc01e7776bedabd8dc9296cf41729710
Leandro Barros <leandroedbarros@gmail.com>
2023-02-05 16:58:37 -0300

    First commit

---
+++ src/test/java/com/example/awss3springservice/AwsS3SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awss3springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3SpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
