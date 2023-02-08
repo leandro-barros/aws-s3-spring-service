5edb44b0ff665a72c89f126baf08aaa53b5c0b5b
Leandro Barros <leandroedbarros@gmail.com>
2023-02-07 21:27:18 -0300

    feat: Method to create bucket S3 AWS

--- src/main/java/com/example/awss3springservice/service/BucketService.java
+++ src/main/java/com/example/awss3springservice/service/BucketService.java
@@ -1,11 +1,17 @@
 package com.example.awss3springservice.service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import com.example.awss3springservice.config.AmazonConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
+@Service
 public class BucketService {
 
     @Autowired
@@ -28,6 +34,23 @@ public class BucketService {
         for (Bucket bucket: buckets) {
             System.out.format("-> %s\n", bucket.getName());
         }
+    }
+
+    public Bucket createBucket(String bucket) {
+        final AmazonS3 s3 = amazonConfig.amazonS3Config();
+        Bucket newBucket = null;
+        boolean doesExist = s3.doesBucketExistV2(bucket);
+        if (doesExist) {
+            newBucket = findBucket(bucket);
+        } else {
+            try {
+                newBucket = s3.createBucket(bucket);
+            } catch (AmazonS3Exception e) {
+                log.info("Error in to create bucket: {}", bucket);
+            }
+        }
+
+        return newBucket;
     }
 
 }
