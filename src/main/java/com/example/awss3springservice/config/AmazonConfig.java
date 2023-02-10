cb9b294047e7d5e2303378e9b71e01c58ce0a8b7
Leandro Barros <leandroedbarros@gmail.com>
2023-02-09 21:03:48 -0300

    feat: Creation variables environment

--- src/main/java/com/example/awss3springservice/config/AmazonConfig.java
+++ src/main/java/com/example/awss3springservice/config/AmazonConfig.java
@@ -5,19 +5,22 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${aws.s3.access-key-id}")
     private String awsAccessKey;
 
+    @Value("${aws.s3.secrect-access-key}")
     private String awsSecretKey;
 
     @Bean
     public AmazonS3 amazonS3Config() {
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("awsAccessKey", awsSecretKey);
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
 
         return AmazonS3ClientBuilder
                 .standard()
